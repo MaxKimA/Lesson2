eb13fbe1b8d8c86044e878686b94103a668d2a13
MaxKimA <maksim0322@gmail.com>
2021-12-24 17:19:54 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(concat(22.6,46));
+
+    }
+    public static  String concat(double temperature,int age){
+        return temperature + " " + age;
+    }
+
+    {
+        int age = false
+        int temperature = 30;
+
+        if
+            System.out.println("Можно идти гулять");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
